af63d4ae718652cca5830d8c2cd3aed65baaeba8
Pranjal Kesarwani <89082511+PranjalKesarwani@users.noreply.github.com>
2023-01-24 23:47:46 +0530

    saw component

--- src/index.js
+++ src/index.js
@@ -1,44 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App'
+
 
 import reportWebVitals from './reportWebVitals';
 
-let currTime = new Date(1995, 11, 17, 7, 24, 0);
-//  let currTime = currGMT.getHours();
-
-const style = {
-  color:"red",
-  alignItems:'center',
-  margin: '0',
-  position: 'absolute',
-  top:'50%'
-}
-
-let wish = '';
-if(currTime>=6 && currTime<12){
-  wish = 'Morning';
-}
-else if(currTime>=12 && currTime<5){
- wish = 'Afternoon'
-}
-else if(currTime>=5 && currTime<7){
-  wish = "Evening"
-}
-else{
-  wish = 'Night'
-}
 
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
 root.render(
   <React.StrictMode>
-       <div >
-        <h1 style={style}>Hello geniuses! Good {wish} </h1>
-       </div>
+
+  <App/>
+   
   </React.StrictMode>
-);
+)
 
 
 
