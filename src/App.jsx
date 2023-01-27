46989012a704cd74a77633103378f9c3dcd625b5
Pranjal Kesarwani <89082511+PranjalKesarwani@users.noreply.github.com>
2023-01-27 19:02:00 +0530

    saw if else

--- src/App.jsx
+++ src/App.jsx
@@ -3,48 +3,71 @@ import Card from './Components/Cards'
 import './index.css';
 import Sdata from './Sdata';
 
+
+const favSeries = "Netflix";
+const FavS = () => {
+
+  // if (favSeries === "Netflix") {
+  //   return (
+  //     <Card
+  //       key={Sdata[0].id}
+  //       imgsrc={Sdata[0].imgsrc}
+  //       sname={Sdata[0].sname}
+  //       title={Sdata[0].title}
+  //       link={Sdata[0].links}
+  //     />
+  //   )
+
+  // } 
+  // else {
+  //   return (
+  //     <Card
+  //       key={Sdata[4].id}
+  //       imgsrc={Sdata[4].imgsrc}
+  //       sname={Sdata[4].sname}
+  //       title={Sdata[4].title}
+  //       link={Sdata[4].links}
+  //     />
+  //   )
+
+  // }
+ return (favSeries==='Netflix' ?  <Card
+  key={Sdata[0].id}
+  imgsrc={Sdata[0].imgsrc}
+  sname={Sdata[0].sname}
+  title={Sdata[0].title}
+  link={Sdata[0].links}
+/>:  <Card
+        key={Sdata[4].id}
+        imgsrc={Sdata[4].imgsrc}
+        sname={Sdata[4].sname}
+        title={Sdata[4].title}
+        link={Sdata[4].links}
+      />)
+}
+
 function App() {
   return (
     <React.StrictMode>
-    <h1 className="heading_style">List of top 5 Netflix Series in 2023</h1>
+      <h1 className="heading_style">List of top 5 Netflix Series in 2023</h1>
 
-    <div className="cards">
+      <div className="cards">
 
-    {
-      Sdata.map((currElem)=>{
-    return    <Card 
-    imgsrc={currElem.imgsrc} 
-    sname={currElem.sname} 
-    title={currElem.title}
-    link={currElem.links}
-    />
-      })
-    }
-       {/* <Card 
-    imgsrc={Sdata[0].imgsrc} 
-    sname={Sdata[0].sname} 
-    title={Sdata[0].title}
-    link={Sdata[0].links}
-    />
-    <Card 
-    imgsrc={Sdata[1].imgsrc} 
-    sname={Sdata[1].sname}
-    title={Sdata[1].title}
-    link={Sdata[1].links} 
-    />
-    <Card 
-    imgsrc={Sdata[2].imgsrc} 
-    sname={Sdata[2].sname} 
-    title={Sdata[2].title} */}
-    {/* link={Sdata[2].links} 
-    />
-    <Card 
-    imgsrc={Sdata[3].imgsrc} 
-    sname={Sdata[3].sname} 
-    title={Sdata[3].title}
-    link={Sdata[3].links} 
-    /> */}
-    </div>
+        {/* {
+          Sdata.map((currElem) => {  //iss call back function ko iss component ke bahar bhi likh sakte the alag se aur zada structured ho jata
+            return <Card
+              key={currElem.id}
+              imgsrc={currElem.imgsrc}
+              sname={currElem.sname}
+              title={currElem.title}
+              link={currElem.links}
+            />
+          })
+        } */}
+
+        <FavS />
+
+      </div>
     </React.StrictMode>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App'
 
-
 import reportWebVitals from './reportWebVitals';
+
+
 
 
 
@@ -16,7 +17,7 @@ root.render(
   <React.StrictMode>
 
   <App/>
-   
+
   </React.StrictMode>
 )
 
